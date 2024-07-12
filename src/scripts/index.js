f499ed9066fbda467a6925c86669f59a296e0050
Petr Chukhnov <peka4u@vk.com>
2024-07-12 20:52:57 +0300

    Third commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -128,6 +128,9 @@ var popupD = document.querySelector(".modal-oz-desktop");
 var openPopupButton = document.querySelector(".button-open-modal-oz");
 var popupD1 = document.querySelector(".modal-zz-desktop");
 var openPopupButtonD1 = document.querySelector(".button-open-modal-zz");
+var pageHeader = document.querySelector(".page_header");
+var main = document.querySelector(".main");
+var footer = document.querySelector(".footer");
 
 var closePopupButton = popup.querySelector(".button-close");
 var closePopupButton1 = popup1.querySelector(".button-close");
@@ -136,86 +139,104 @@ var closePopupButtonL = popupL.querySelector(".button-close");
 var closePopupButtonL1 = popupL1.querySelector(".button-close");
 var closePopupButtonD = popupL.querySelector(".button-close");
 var closePopupButtonD1 = popupL1.querySelector(".button-close");
-var pageHeader = document.querySelector(".page_header");
-var main = document.querySelector(".main");
-var footer = document.querySelector(".footer");
 
 openPopupButton.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-oz--show");
-  pageHeader.classList.add("blurred");
-  main.classList.add("blurred");
-  footer.classList.add("blurred");
+  document.querySelector(".main").style.filter = "blur(5px)";
+  document.querySelector(".page__header").style.filter = "blur(5px)";
+  document.querySelector(".footer").style.filter = "blur(5px)";
 });
 closePopupButton.addEventListener("click", function () {
   popup.classList.remove("modal-oz--show");
+  document.querySelector(".main").style.filter = "none";
+  document.querySelector(".page__header").style.filter = "none";
+  document.querySelector(".footer").style.filter = "none";
 });
 
 openPopupButton1.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup1.classList.add("modal-zz--show");
-  pageHeader.classList.add("blurred");
-  main.classList.add("blurred");
-  footer.classList.add("blurred");
+
+  document.querySelector(".main").style.filter = "blur(5px)";
+  document.querySelector(".page__header").style.filter = "blur(5px)";
+  document.querySelector(".footer").style.filter = "blur(5px)";
 });
 closePopupButton1.addEventListener("click", function () {
   popup1.classList.remove("modal-zz--show");
+  document.querySelector(".main").style.filter = "none";
+  document.querySelector(".page__header").style.filter = "none";
+  document.querySelector(".footer").style.filter = "none";
 });
 
 openPopupButton0.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup0.classList.add("block0--show");
-  pageHeader.classList.add("blurred");
-  main.classList.add("blurred");
-  footer.classList.add("blurred");
+
+  document.querySelector(".main").style.filter = "blur(5px)";
+  document.querySelector(".page__header").style.filter = "blur(5px)";
+  document.querySelector(".footer").style.filter = "blur(5px)";
 });
 
 closePopupButton0.addEventListener("click", function () {
   popup0.classList.remove("block0--show");
+  document.querySelector(".main").style.filter = "none";
+  document.querySelector(".page__header").style.filter = "none";
+  document.querySelector(".footer").style.filter = "none";
 });
 
 openPopupButtonL.addEventListener("click", function (evt) {
   evt.preventDefault();
   popupL.classList.add("modal-oz--show");
-  pageHeader.classList.add("blurred");
-  main.classList.add("blurred");
-  footer.classList.add("blurred");
+  document.querySelector(".main").style.filter = "blur(5px)";
+  document.querySelector(".page__header").style.filter = "blur(5px)";
+  document.querySelector(".footer").style.filter = "blur(5px)";
 });
 closePopupButtonL.addEventListener("click", function () {
   popupL.classList.remove("modal-oz--show");
+  document.querySelector(".main").style.filter = "none";
+  document.querySelector(".page__header").style.filter = "none";
+  document.querySelector(".footer").style.filter = "none";
 });
 
 openPopupButtonL1.addEventListener("click", function (evt) {
   evt.preventDefault();
   popupL1.classList.add("modal-oz--show");
-  pageHeader.classList.add("blurred");
-  main.classList.add("blurred");
-  footer.classList.add("blurred");
+  document.querySelector(".main").style.filter = "blur(5px)";
+  document.querySelector(".page__header").style.filter = "blur(5px)";
+  document.querySelector(".footer").style.filter = "blur(5px)";
 });
 closePopupButtonL1.addEventListener("click", function () {
   popupL1.classList.remove("modal-oz--show");
+  document.querySelector(".main").style.filter = "none";
+  document.querySelector(".page__header").style.filter = "none";
+  document.querySelector(".footer").style.filter = "none";
 });
 
 openPopupButtonD.addEventListener("click", function (evt) {
   evt.preventDefault();
   popupD.classList.add("modal-oz--show");
-  pageHeader.classList.add("blurred");
-  main.classList.add("blurred");
-  footer.classList.add("blurred");
+  document.querySelector(".main").style.filter = "blur(5px)";
+  document.querySelector(".page__header").style.filter = "blur(5px)";
+  document.querySelector(".footer").style.filter = "blur(5px)";
 });
 closePopupButtonD.addEventListener("click", function () {
   popupD.classList.remove("modal-oz--show");
+  document.querySelector(".main").style.filter = "none";
+  document.querySelector(".page__header").style.filter = "none";
+  document.querySelector(".footer").style.filter = "none";
 });
 
 openPopupButtonD1.addEventListener("click", function (evt) {
   evt.preventDefault();
   popupD1.classList.add("modal-oz--show");
-  pageHeader.classList.add("blurred");
-  main.classList.add("blurred");
-  footer.classList.add("blurred");
+  document.querySelector(".overlay").style.display = "block";
 });
 closePopupButtonD1.addEventListener("click", function () {
   popupD1.classList.remove("modal-oz--show");
+  document.querySelector(".main").style.filter = "none";
+  document.querySelector(".page__header").style.filter = "none";
+  document.querySelector(".footer").style.filter = "none";
 });
 
 window.onload = function () {
